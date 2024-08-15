fa9f7339c1a0eb1c38e6e090896b02023a0206ee
GabrielG246 <garcia-gabriel-246@hotmail.com>
2024-08-14 23:07:03 -0300

    Creación: Ventana Principal

---
+++ main.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+def main():
+    ventana= tk.Tk()
+    ventana.title('Generador de Contraseña')
+
+    ventana.state('zoomed')
+
+    ventana.mainloop()
+
+
+if __name__=='__main__':
+    main()
